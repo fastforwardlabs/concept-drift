95b45385912e9464f4ee78c6c7a8d6ac02bb2678
Andrew Reed <andrew.reed.r@gmail.com>
2021-07-16 09:07:46 -0400

    add multiple plotting utility

--- test_harness/utils/utils.py
+++ test_harness/utils/utils.py
@@ -1,14 +1,28 @@
 import pandas as pd
 
 
-def plot_experiment_error(experiment):
+def format_experimental_scores(experiment):
     return (
         pd.DataFrame(experiment.experiment_metrics["scores"])
         .set_index(0)
-        .plot(
-            figsize=(15, 7),
-            title=f"Cumulative Score over Experiment \n \
+        .rename(columns={1: experiment.name})
+    )
+
+
+def plot_experiment_error(experiment):
+    scores_df = format_experimental_scores(experiment)
+    return scores_df.plot(
+        figsize=(15, 7),
+        title=f"Cumulative Score over Experiment \n \
             Label Expense: {experiment.experiment_metrics['label_expense']['num_labels_requested']} \n \
             Total Train Time: {experiment.experiment_metrics['total_train_time']}",
-        )
+    )
+
+
+def plot_multiple_experiments(experiments):
+
+    exp_dfs = [format_experimental_scores(experiment) for experiment in experiments]
+
+    return pd.concat(exp_dfs, axis=1).plot(
+        figsize=(15, 7), title="Overall Score by Experiment"
     )
