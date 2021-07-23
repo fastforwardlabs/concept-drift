4fbff0af8682735a38ed70560b9a423a2793eb04
Andrew Reed <andrew.reed.r@gmail.com>
2021-07-23 16:52:52 -0400

    Add margin uncertainty experiment

---
+++ test_harness/experiments/margin_uncertainty_experiment.py
@@ -0,0 +1,220 @@
+import logging
+
+import numpy as np
+import pandas as pd
+from scipy.stats import ks_2samp, chisquare
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import StratifiedKFold, LeaveOneOut
+
+from test_harness.experiments.baseline_experiment import BaselineExperiment
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+file_handler = logging.FileHandler("../logs/app.log")
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
+
+
+class UncertaintyX2Experiment(BaselineExperiment):
+    def __init__(
+        self, model, dataset, k, significance_thresh, margin_width, param_grid=None
+    ):
+        super().__init__(model, dataset, param_grid)
+        self.name = "uncertainty-x2"
+        self.k = k
+        self.significance_thresh = significance_thresh
+        self.ref_distributions = []
+        self.ref_margins = []
+        self.det_distributions = []
+        self.det_margins = []
+        self.p_vals = []
+        self.margin_width = margin_width
+
+    @staticmethod
+    def make_kfold_predictions(X, y, model, k):
+        """A KFold version of LeaveOneOut predictions.
+
+        Rather than performing exhaustive leave-one-out methodology to get predictions
+        for each observation, we use a less exhaustive KFold approach.
+
+        When k == len(X), this is equivalent to LeaveOneOut: expensive, but robust. Reducing k
+        saves computation, but reduces robustness of model.
+
+        Args:
+            X (pd.Dataframe) - features in evaluation window
+            y (pd.Series) - labels in evaluation window
+            k (int) - number of folds
+            type (str) - specified kfold or LeaveOneOut split methodology
+
+        Returns:
+            preds (np.array) - an array of predictions for each X in the input (NOT IN ORDER OF INPUT)
+
+        """
+
+        splitter = StratifiedKFold(n_splits=k, random_state=42, shuffle=True)
+
+        preds = np.array([])
+        pred_margins = np.array([])
+
+        for train_indicies, test_indicies in splitter.split(X, y):
+
+            pipe = Pipeline(
+                steps=[
+                    ("scaler", MinMaxScaler()),
+                    ("clf", model),
+                ]
+            )
+
+            # fit it
+            pipe.fit(X.iloc[train_indicies], y.iloc[train_indicies])
+
+            # score it on this Kfold's test data
+            y_preds_split = pipe.predict_proba(X.iloc[test_indicies])
+
+            # get positive class prediction
+            y_preds_split_posclass_proba = y_preds_split[:, 1]
+            preds = np.append(preds, y_preds_split_posclass_proba)
+
+            # get pred margins
+            # https://github.com/SeldonIO/alibi-detect/blob/86dc3148ee5a3726fb6229d5369c38e7e97b6040/alibi_detect/cd/preprocess.py#L49
+            top_2_probs = -np.partition(-y_preds_split, kth=1, axis=-1)
+            diffs = top_2_probs[:, 0] - top_2_probs[:, 1]
+            pred_margins = np.append(pred_margins, diffs)
+
+        return preds, pred_margins
+
+    def get_reference_response_distribution(self):
+
+        # get data in reference window
+        window_idx = self.reference_window_idx
+        print(f"GETTING REFERENCE DISTRIBUTION FOR WINDOW: {window_idx}")
+        X_train, y_train = self.dataset.get_window_data(window_idx, split_labels=True)
+
+        # perform kfoldsplits to get predictions
+        preds, pred_margins = self.make_kfold_predictions(
+            X_train, y_train, self.model, self.k
+        )
+
+        return preds, pred_margins
+
+    def get_detection_response_distribution(self):
+
+        # get data in prediction window
+        window_idx = self.detection_window_idx
+        print(f"GETTING DETECTION DISTRIBUTION FOR WINDOW: {window_idx}")
+        X_test, y_test = self.dataset.get_window_data(window_idx, split_labels=True)
+
+        # use trained model to get response distribution
+        y_preds_split = self.trained_model.predict_proba(X_test)
+        preds = y_preds_split[:, 1]
+
+        # get pred margins
+        # https://github.com/SeldonIO/alibi-detect/blob/86dc3148ee5a3726fb6229d5369c38e7e97b6040/alibi_detect/cd/preprocess.py#L49
+        top_2_probs = -np.partition(-y_preds_split, kth=1, axis=-1)
+        pred_margins = top_2_probs[:, 0] - top_2_probs[:, 1]
+
+        return preds, pred_margins
+
+    @staticmethod
+    def perform_ks_test(dist1, dist2):
+        return ks_2samp(dist1, dist2)
+
+    def run(self):
+        """Response Margin Uncertainty Experiment
+
+        This experiment uses a X2 test to detect changes in the margin of the target/response distribution between
+        the reference and detection windows.
+
+        Logic flow:
+            - Train on initial reference window
+            - Perform Stratified KFold to obtain prediction distribution on reference window
+            - Use trained model to generate predictions on detection window
+            - Calculate the difference between confidence values of binary classes for each observation in both windows (aka margin)
+            - Use the specified margin threshold (e.g. 0.1 for [0.45, 0.55]) to assign binary class to each observation (e.g. in or out of margin)
+            - Perform Chi-Squared Goodness of Fit Test between reference and detection window response margins
+                - If different, retrain and update both windows
+                - If from same distribution, update detection window and repeat
+
+        """
+        logger.info(
+            f"-------------------- Started Response Margin Uncertainty Experiment Run --------------------"
+        )
+        self.train_model_gscv(window="reference", gscv=True)
+
+        CALC_REF_RESPONSE = True
+
+        for i, split in enumerate(self.dataset.splits):
+
+            if i > self.reference_window_idx:
+                print(f"Dataset index of split end: {self.dataset.splits[i]}")
+
+                logger.info(
+                    f"Need to calculate Reference response distribution? - {CALC_REF_RESPONSE}"
+                )
+
+                # log actual score on detection window
+                self.experiment_metrics["scores"].extend(
+                    self.evaluate_model_incremental(n=10)
+                )
+
+                # get reference window response distribution with kfold + detection response distribution
+                if CALC_REF_RESPONSE:
+                    (
+                        ref_response_dist,
+                        ref_response_margins,
+                    ) = self.get_reference_response_distribution()
+                (
+                    det_response_dist,
+                    det_response_margins,
+                ) = self.get_detection_response_distribution()
+
+                self.ref_distributions.append(ref_response_dist)
+                self.ref_margins.append(ref_response_margins)
+
+                self.det_distributions.append(det_response_dist)
+                self.det_margins.append(det_response_margins)
+
+                # compare change in margin use Chi Squared test for goodness of fit
+                ref_uncertainties = (ref_response_margins < self.margin_width).astype(
+                    int
+                )
+                det_uncertainties = (det_response_margins < self.margin_width).astype(
+                    int
+                )
+
+                expected = (
+                    pd.Series(ref_uncertainties).value_counts(normalize=False).tolist()
+                )
+                observed = (
+                    pd.Series(det_uncertainties).value_counts(normalize=False).tolist()
+                )
+
+                x2_result = chisquare(f_obs=observed, f_exp=expected)
+
+                self.p_vals.append(x2_result.pvalue)
+
+                logger.info(f"KS Test: {x2_result}")
+
+                if x2_result[1] < self.significance_thresh:
+                    # reject null hyp, distributions are NOT same --> retrain
+                    self.train_model_gscv(window="detection", gscv=True)
+                    self.update_reference_window()
+                    CALC_REF_RESPONSE = True
+                    _ks_result_report = "FAILED"
+                else:
+                    CALC_REF_RESPONSE = False
+                    _ks_result_report = "PASSED"
+
+                self.update_detection_window()
+
+                print(f"X2 Test Result: {_ks_result_report} | {x2_result}")
+                print()
+
+        self.calculate_label_expense()
+        self.calculate_train_expense()
