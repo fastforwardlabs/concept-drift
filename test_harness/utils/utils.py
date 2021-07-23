b102544305046ac1976afd08134b10ee268fbfb0
Andrew Reed <andrew.reed.r@gmail.com>
2021-07-22 20:40:50 -0400

    Update utils.py

--- test_harness/utils/utils.py
+++ test_harness/utils/utils.py
@@ -12,14 +12,31 @@ def format_experimental_scores(experiment):
     )
 
 
-def plot_experiment_error(experiment):
+def plot_experiment_error(experiment, retrain_points=True):
     scores_df = format_experimental_scores(experiment)
-    return scores_df.plot(
+    ax = scores_df.plot(
         figsize=(15, 7),
         title=f"Cumulative Score over Experiment \n \
             Label Expense: {experiment.experiment_metrics['label_expense']['num_labels_requested']} \n \
             Total Train Time: {experiment.experiment_metrics['total_train_time']}",
     )
+
+    retrainings = [
+        experiment.dataset.get_split_idx(window_idx)
+        for window_idx in [
+            train_record["window_idx"]
+            for train_record in experiment.experiment_metrics["training"]
+        ]
+    ]
+
+    if retrain_points:
+        [
+            ax.axvline(i, color="black", linestyle=":", linewidth=0.75)
+            for i in retrainings
+            if i != 0
+        ]
+
+    plt.show()
 
 
 def plot_multiple_experiments(experiments, change_points=None):
@@ -76,3 +93,19 @@ def plot_response_distributions_bysplit(sqsi_exp):
     g = sns.FacetGrid(df_melt, col="Split", hue="Window Type", col_wrap=4)
     g.map_dataframe(sns.kdeplot, "value", fill=True)
     g.add_legend()
+
+
+def calculate_distances_window_distances(experiment, distance_func):
+
+    splits = []
+
+    for i in range(len(experiment.ref_distributions)):
+
+        ref_dist = experiment.ref_distributions[i]
+        det_dist = experiment.det_distributions[i]
+
+        distance = distance_func(ref_dist, det_dist)
+
+        splits.append((i, distance))
+
+    return pd.DataFrame(splits, columns=["Split", "Distance"])
